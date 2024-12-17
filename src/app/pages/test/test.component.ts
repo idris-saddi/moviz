b3489b0e665686482767a66f867e2e1c69827b79
salma <salmakchaw123@gmail.com>
2024-12-17 19:08:02 +0100

    added the rate style

--- src/app/pages/test/test.component.ts
+++ src/app/pages/test/test.component.ts
@@ -3,10 +3,11 @@ import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { SegementedControlComponent } from "../../components/segmented-control/segmented-control.component";
 import { SegmentedControlConfig } from '../../interfaces/ui-configs/segemented-control-config.interface';
 import { InputComponent } from "../../components/input/input.component";
+import { RateChipComponent } from "../../components/rate-chip/rate-chip.component";
 
 @Component({
   selector: 'app-test',
-  imports: [NavbarComponent, SegementedControlComponent, InputComponent],
+  imports: [NavbarComponent, SegementedControlComponent, InputComponent, RateChipComponent],
   templateUrl: './test.component.html',
   styleUrl: './test.component.css'
 })
